c31b40679251d359f644b2cd86169545e3c1abb2
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-07-29 20:02:06 +0100

    Fonctionnalité d'ajout de nouveau post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,31 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def new
+    @post = Post.new
+  end
+  def confirm
+    @post = Post.new(post_params)
+    if @post.invalid?
+      render :new 
+    end
+  end
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "J'ai créé un post!"
+      else
+        # Redessinez le formulaire de saisie.
+        render :new
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
